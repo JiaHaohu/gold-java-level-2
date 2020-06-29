a40f325870b91c963c97e4b511d09cee6e19d025
Liu Xia <lxconan@gmail.com>
2020-06-29 10:49:21 +0800

    [liuxia] docs: update java doc for repository.

--- src/main/java/com/tw/domain/TodoItemRepository.java
+++ src/main/java/com/tw/domain/TodoItemRepository.java
@@ -9,18 +9,38 @@ public class TodoItemRepository {
         this.configuration = configuration;
     }
 
+    /**
+     * Create an item and save it into database. The `checked` status should default to `false`.
+     *
+     * @param name The name of the item.
+     * @return The `id` of the item created.
+     * @throws IllegalArgumentException The name is `null`.
+     */
     public long create(String name) throws Exception {
         // TODO:
         //   Please implement the method.
         throw new RuntimeException("Delete me");
     }
 
+    /**
+     * Change item checked status to specified state.
+     *
+     * @param id The item to update.
+     * @param checked The new checked status.
+     * @return If the item exist returns true, otherwise returns false.
+     */
     public boolean changeCheckedStatus(long id, boolean checked) throws Exception {
         // TODO:
         //   Please implement the method.
         throw new RuntimeException("Delete me");
     }
 
+    /**
+     * Get item by its id.
+     *
+     * @param id The id of the item.
+     * @return The item entity. If the item does not exist, returns empty.
+     */
     public Optional<TodoItem> findById(long id) throws Exception {
         // TODO:
         //   Please implement the method.
